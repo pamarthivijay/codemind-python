2d42ce35e2939a204e097cab3afff7c200f774b0
pamarthivijay <114654091+pamarthivijay@users.noreply.github.com>
2022-12-07 15:09:40 +0530

    n=int(input()) l=list(map(int,input().split())) d=[] for i in l:     c=0     for j in range(2,i+1):         if i%j==0:             c+=1     if c==1:         d.append(i) a=sum(d)/len(d) print("%.2f"%a)

---
+++ Average_of_primes.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+d=[]
+for i in l:
+    c=0
+    for j in range(2,i+1):
+        if i%j==0:
+            c+=1
+    if c==1:
+        d.append(i)
+a=sum(d)/len(d)
+print("%.2f"%a)
+        
+        
