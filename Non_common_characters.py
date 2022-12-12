c247411d2e719ea2c3e52550de48552e073e8829
pamarthivijay <114654091+pamarthivijay@users.noreply.github.com>
2022-12-12 15:53:55 +0530

    a=input() b=a.lower() x=input() y=x.lower() l=[] l1=[] for i in b:     if i not in y and i!=' ':         l.append(i) for i in y:     if i not in b:         l.append(i) q=set(l) q=list(q) q.sort() print(len(q))

---
+++ Non_common_characters.py
@@ -0,0 +1,16 @@
+a=input()
+b=a.lower()
+x=input()
+y=x.lower()
+l=[]
+l1=[]
+for i in b:
+    if i not in y and i!=' ':
+        l.append(i)
+for i in y:
+    if i not in b:
+        l.append(i)
+q=set(l)
+q=list(q)
+q.sort()
+print(len(q))
